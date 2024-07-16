979749d3c3c2428cfb96afba5f89d6831a0dfa12
JamesZam96 <jameszamgm@gmail.com>
2024-07-16 11:20:25 -0500

    adminView

--- resources/views/components/nav-bar-company.blade.php
+++ resources/views/components/nav-bar-company.blade.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 <link rel="stylesheet" href="{{ asset('css/sidebar-company.css') }}">
 <nav class="navbar navbar-expand-lg navbar-light py-4 px-4 bg-transparent">
     <div class="d-flex align-items-center">
@@ -24,3 +26,5 @@
         </ul>
     </div>
 </nav>
+<script src="{{asset('js/sidebar-company.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
